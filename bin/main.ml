1554a3e005f6e15e5190513ea289c6be566b4b2b
Luke Kulm <lbk73@cornell.edu>
2023-03-19 19:21:39 -0400

    Put in start code for graphics window

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,10 @@
 open Graphics
-open Graphics
 
 let main () =
-  open_graph "";
-  close_graph ()
+  open_graph " 600X600";
+  moveto 0 0;
+  draw_string "Resize window, then press a key to start...";
+  ignore (read_key () : char);
+  clear_graph ()
 
 let _ = main ()
