54fed570f2dc375807d8dae2d3eccbfcd78a7f09
Henry Rogers <hwr34@cornell.edu>
2023-03-19 17:20:02 -0400

    Fix some dune and opam missing pieces. Fix module syntax

--- _build/default/src/cell.mli
+++ _build/default/src/cell.mli
@@ -1,18 +1,16 @@
-module type Cell = sig
-  type t
-  (** The abstract type of one cell in the grid of the game of life*)
+(* module Cell = sig *)
+type t
+(** The abstract type of one cell in the grid of the game of life*)
 
-  val init_cell : bool -> int * int -> t list -> t
-  (** Initializes a new cell of type t, alive if bool = true, dead if bool =
-      false*)
+val init_cell : bool -> int * int -> t list -> t
+(** Initializes a new cell of type t, alive if bool = true, dead if bool = false*)
 
-  val get_alive : t -> bool
-  (** xxxx *)
+val get_alive : t -> bool
+(** xxxx *)
 
-  val cell_update : t -> t
-  (** xxxx do we need this if we are not adding position locations to the cell?
-      we would just re initialize. so should we add x and y points to the cell?
-      design question! *)
+val cell_update : t -> t
+(** xxxx do we need this if we are not adding position locations to the cell? we
+    would just re initialize. so should we add x and y points to the cell?
+    design question! *)
 
-  exception NotImplemented
-end
+exception NotImplemented
---
+++ _build/install/default/lib/Life/life.ml
@@ -0,0 +1,1 @@
+../../../../default/src/life.ml-gen
