59543024e5f2bccbf73eb8d0a331940d33dce12f
dcv26 <dcv26@cornell.edu>
2023-03-19 17:37:17 -0400

    Removed errors in the main.ml file and added one unit test case for Cell
    - Debugging

--- test/main.ml
+++ test/main.ml
@@ -11,11 +11,11 @@ let update_cell_test_bool (name : string) (input : Cell.t)
     (expected_output : bool) : test =
   name >:: fun _ ->
   assert_equal expected_output
-    (input |> Cell.update_cell input |> Cell.get_alive)
+    (input |> Cell.cell_update |> Cell.get_alive)
     ~printer:string_of_bool
 
 (* should we also test that the neighbors and position didn't change? if so: let
    update_cell_test_neighbors = . . . . . *)
-let cell_tests = [ get_alive_test "" ]
+let cell_tests = [ get_alive_test "name" (Cell.init_cell true (0, 0) []) true ]
 let suite = "test suite for Final Project" >::: List.flatten [ cell_tests ]
 let _ = run_test_tt_main suite
