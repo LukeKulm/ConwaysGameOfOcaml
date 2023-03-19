42468596b35f23037897213f933c9d2cb4034756
dcv26 <dcv26@cornell.edu>
2023-03-19 17:00:40 -0400

    Added new tests and restructured Cell.ml
    - Removed "struct" and "sig" signatures for module Cell

--- test/main.ml
+++ test/main.ml
@@ -3,8 +3,17 @@ open OUnit2
 let get_alive_test (name : string) (input : Cell.t) (expected_output : bool) :
     test =
   name >:: fun _ ->
-  assert_equal expected_output (Cell.get_alive input) ~printer:String.escaped
+  assert_equal expected_output (Cell.get_alive input) ~printer:string_of_bool
 
-let cell_tests = [ get_alive_test ]
-let suite = "test suite for A2" >::: List.flatten [ cell_tests ]
+let update_cell_test_bool (name : string) (input : Cell.t)
+    (expected_output : bool) : test =
+  name >:: fun _ ->
+  assert_equal expected_output
+    (input |> Cell.update_cell input |> Cell.get_alive)
+    ~printer:string_of_bool
+
+(* should we also test that the neighbors and position didn't change? if so: let
+   update_cell_test_neighbors = . . . . . *)
+let cell_tests = [ get_alive_test "" ]
+let suite = "test suite for Final Project" >::: List.flatten [ cell_tests ]
 let _ = run_test_tt_main suite
