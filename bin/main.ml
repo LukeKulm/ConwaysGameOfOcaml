2cd6dcfe86c87f5fb26c43cfa42fc2180f75593e
Luke Kulm <lbk73@cornell.edu>
2023-03-20 16:50:50 -0400

    Continued work on gui

--- bin/main.ml
+++ bin/main.ml
@@ -2,10 +2,15 @@ open Graphics
 open Life.Display
 
 let rec animate () =
-  open_graph " 500X500";
+  open_graph
+    (" "
+    ^ string_of_int (fst window_size)
+    ^ "X"
+    ^ string_of_int (snd window_size));
   set_window_title "The Game of Life";
-  Unix.sleepf 0.05;
-  (* Wait for a short time before updating the cells again *)
+  draw_string "Press a key when your happy with the start (;";
+  Unix.sleepf 0.25;
   animate ()
+(* Wait for a short time before updating the cells again *)
 
 let _ = animate ()
