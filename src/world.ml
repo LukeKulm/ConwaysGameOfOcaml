12c4bbd88864c113783d7aa7f85d9ab3965eb47c
dcv26 <dcv26@cornell.edu>
2023-03-21 14:45:00 -0400

    Attempted to implement World.ml
    - Am pushing so that Hank can continue to work

--- src/world.ml
+++ src/world.ml
@@ -10,32 +10,37 @@ type t = {
   cells : Cell.t list list;
 }
 
+let get_dims grid = (grid.cols, grid.rows)
+
 let rec in_selected lst pt =
   List.filter (fun s -> if s = pt then true else false) lst
 
 let make_neighbors pt = raise NotImplemented
 let init_row = raise NotImplemented
 
-let init_cells col row =
+let rec init_matrix (col : int list) (row : int list) (acc : int list list) =
   match row with
-  | h :: t -> []
-  | [] -> []
-(* use map!!!!! *)
-(*col and row are each lists up to that int*)
-(* essentially want a for loop: step 1: make a list of cell objects with empty
-   neighbor lists that is col long step 2: call map on this list with function
-   make_neighbors*)
+  | h :: t -> init_matrix col t (col :: acc)
+  | [] -> acc
 
 let rec list_upto_helper i n =
   if i = n then [] else i :: list_upto_helper (i + 1) n
 
 let list_upto n = list_upto_helper 0 n
 
+let rec init_cell_helper matrix row_counter acc =
+  match matrix with
+  | h :: t -> (
+      match h with
+      | h :: t -> Cell.init_cell false (h, row_counter) [] :: acc
+      | [] -> init_cell_helper t (row_counter + 1))
+  | [] -> acc
+
+(* essentially just want to call Cell.init_cell, passing the element's row and
+   column as the indices*)
+let init_cells col row = init_cell_helper (init_matrix col row []) 0 []
+
 let init_world c r =
   { cols = c; rows = r; cells = init_cells (list_upto c) (list_upto r) }
 
-(* option: make a list list of the right dimensions with all ones, and then use
-   map to actually do the right thing?*)
-(* for all BUT the points in the list , we want to initialize as FALSE, so: if
-   in_selected . . . true, else . . . false*)
 let world_update = raise NotImplemented
