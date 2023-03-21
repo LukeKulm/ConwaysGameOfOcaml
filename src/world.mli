3f32919554729012c8d311758cc014fb7506a467
Henry Rogers <hwr34@cornell.edu>
2023-03-21 15:13:02 -0400

    new branch worldv2

--- src/world.mli
+++ src/world.mli
@@ -1,5 +1,5 @@
 type t
-(*abstract type . . . *)
+(* abstract type . . . *)
 
 val init_world : int -> int -> (int * int) list -> t
 (* Initializes a world that is of x by y dimensions, where the int by int list
@@ -9,11 +9,11 @@ val get_dims : t -> int * int
 (* returns # of rows by # of cols*)
 
 val get_which_alive : t -> (int * int) list
-(*returns the coordinates of which points are alive within the grid, with (0,0)
-  being the top left corner *)
+(* returns the coordinates of which points are alive within the grid, with (0,0)
+   being the top left corner *)
 
 val world_update : t -> t
 (* Update the world by calling upon each cell to update itself: important to
-   note that the order in whcih we do so matters*)
+   note that the order in which we do so matters*)
 
 exception NotImplemented
---
+++ src/world2.ml
@@ -0,0 +1,46 @@
+(* to create the list of neighbors for cell at pos (x,y) in array a, here are
+   the indices of who we want to check: a[x][y+1] a[x][y-1] a[x+1][y+1]
+   a[x+1][y-1] a[x-1][y] a[x+1][y] a[x-1][y-1] a[x-1][y+1]*)
+
+exception NotImplemented
+
+type t = {
+  cols : int;
+  rows : int;
+  cells : Cell.t list list;
+}
+
+let get_dims grid = (grid.cols, grid.rows)
+
+let rec in_selected lst pt =
+  List.filter (fun s -> if s = pt then true else false) lst
+
+let make_neighbors pt = raise NotImplemented
+let init_row = raise NotImplemented
+
+let rec init_matrix (col : int list) (row : int list) (acc : int list list) =
+  match row with
+  | h :: t -> init_matrix col t (col :: acc)
+  | [] -> acc
+
+let rec list_upto_helper i n =
+  if i = n then [] else i :: list_upto_helper (i + 1) n
+
+let list_upto n = list_upto_helper 0 n
+
+let rec init_cell_helper matrix row_counter acc =
+  match matrix with
+  | h :: t -> (
+      match h with
+      | h :: t -> Cell.init_cell false (h, row_counter) [] :: acc
+      | [] -> init_cell_helper t (row_counter + 1))
+  | [] -> acc
+
+(* essentially just want to call Cell.init_cell, passing the element's row and
+   column as the indices*)
+let init_cells col row = init_cell_helper (init_matrix col row []) 0 []
+
+let init_world c r =
+  { cols = c; rows = r; cells = init_cells (list_upto c) (list_upto r) }
+
+let world_update = raise NotImplemented
