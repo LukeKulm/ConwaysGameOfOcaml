75e730380649fe7fa9bccba26f4efae80e395267
dcv26 <dcv26@cornell.edu>
2023-03-20 21:13:08 -0400

    Worked on World interface and implementation
    - Added two "getter" functions to world interface
    - Explored but did not finish implementation

--- src/world.ml
+++ src/world.ml
@@ -13,8 +13,26 @@ type t = {
 let rec in_selected lst pt =
   List.filter (fun s -> if s = pt then true else false) lst
 
+let make_neighbors pt = raise NotImplemented
 let init_row = raise NotImplemented
-let init_world = raise NotImplemented
+
+let init_cells col row =
+  match row with
+  | h :: t -> []
+  | [] -> []
+(* use map!!!!! *)
+(*col and row are each lists up to that int*)
+(* essentially want a for loop: step 1: make a list of cell objects with empty
+   neighbor lists that is col long step 2: call map on this list with function
+   make_neighbors*)
+
+let rec list_upto_helper i n =
+  if i = n then [] else i :: list_upto_helper (i + 1) n
+
+let list_upto n = list_upto_helper 0 n
+
+let init_world c r =
+  { cols = c; rows = r; cells = init_cells (list_upto c) (list_upto r) }
 
 (* option: make a list list of the right dimensions with all ones, and then use
    map to actually do the right thing?*)
--- src/world.mli
+++ src/world.mli
@@ -5,6 +5,13 @@ val init_world : int -> int -> (int * int) list -> t
 (* Initializes a world that is of x by y dimensions, where the int by int list
    are the coordinates of the originally alive cells *)
 
+val get_dims : t -> int * int
+(* returns # of rows by # of cols*)
+
+val get_which_alive : t -> (int * int) list
+(*returns the coordinates of which points are alive within the grid, with (0,0)
+  being the top left corner *)
+
 val world_update : t -> t
 (* Update the world by calling upon each cell to update itself: important to
    note that the order in whcih we do so matters*)
