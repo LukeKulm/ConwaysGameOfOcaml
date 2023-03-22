c096ddabd1597aab224b4e2ba9774a440f192c2a
Henry Rogers <hwr34@cornell.edu>
2023-03-22 10:57:20 -0400

    Clean up documentation
    - Adjusted some of the comments in our .mli files to conform with
    the course format and be more clear.
    - Also changed the makefile to name our zip file life.zip

--- src/display.ml
+++ src/display.ml
@@ -10,7 +10,7 @@ let window_size state =
   (cell_size * fst (get_dims state), cell_size * snd (get_dims state))
 
 let rows state = fst (window_size state) / cell_size
-let collums state = snd (window_size state) / cell_size
+let columns state = snd (window_size state) / cell_size
 
 let draw_cell num1 num2 =
   fill_rect (num1 * cell_size) (num2 * cell_size) cell_size cell_size
--- src/world.mli
+++ src/world.mli
@@ -1,23 +1,23 @@
 type t
-(* abstract type . . . *)
+(* [t] is the abstract type for the world. *)
 
 val init_world : int -> int -> t
 (* [init_world width height] Initializes a world with all dead cells that is
-   [width] wide and [height] tall *)
+   [width] wide and [height] tall. *)
 
 val init_world_with_alive : int -> int -> (int * int) list -> t
 (* [init_world_with_cells width height living] Initializes a world that is of
    [width] wide and [height] tall and sets all cells at coordinates specified by
-   [living] to be alive. All other cells are dead *)
+   [living] to be alive. All other cells are dead. *)
 
 val get_dims : t -> int * int
-(* [get_dims world] returns the dimensions of [world] *)
+(* [get_dims world] returns the dimensions of [world]. *)
 
 val get_alive : t -> (int * int) list
 (* [get_alive world] returns the list of coordinates of which points are alive
-   within [world], with (0,0) being the top left corner *)
+   within [world], with (0,0) being the top left corner. *)
 
 val update_world : t -> t
-(* [update_world world] updates [world] according to its current state *)
+(* [update_world world] updates [world] according to its current state. *)
 
 exception NotImplemented
