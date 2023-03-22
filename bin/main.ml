c5fdb2dd1930670610bcb6c194dd7de1c5c526e8
Luke Kulm <lbk73@cornell.edu>
2023-03-22 01:03:53 -0400

    Continued implementing bin/main.ml -now waits for a key to be pressed to start animation

--- bin/main.ml
+++ bin/main.ml
@@ -5,17 +5,19 @@ open Life.World
 let rec animate state =
   clear_graph ();
   draw_frame state;
-  Unix.sleepf 0.5;
+  Unix.sleepf 0.3;
   animate (update_world state)
 
-let start () =
+let rec start () =
   open_graph
     (" "
     ^ string_of_int (fst (window_size initial))
     ^ "X"
     ^ string_of_int (snd (window_size initial)));
   set_window_title "The Game of Life";
-  animate initial
+  draw_string "Press a Key When You're Happy";
+  if (wait_next_event [ Key_pressed ]).keypressed then animate initial
+  else start ()
 (* Wait for a short time before updating the cells again *)
 
 let _ = start ()
